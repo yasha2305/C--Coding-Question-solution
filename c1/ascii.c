5bed3a06cbc4728aee425aea760218d620297b4a
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-05 16:09:00 +0530

    question

---
+++ c1/ascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    char c;
+    printf("Enter the Character:");
+    scanf("%c",&c);
+    printf("The ASCII Value of the given character is %d",c);
+    return 0;
+
+
+
+}
