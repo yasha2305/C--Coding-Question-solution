45bf1a9e0b7b36e86f92f5ef3ef2a4b0122737c3
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-10 22:18:58 +0530

    Do-While loop

--- loops.c/basicdw.c
+++ loops.c/basicdw.c
@@ -1,15 +1,17 @@
-// printing table of two using while loop
+// do while
+// This program will print the table of two using do while
 #include <stdio.h>
 int main()
 {
-    int i = 1; // In while loop initialization is must
+    int i=1; // note in do while loop intialization is must
     printf("The Table of Two is:\n");
-    while (i < 11)
+
+    do
     {
         int ans;
         ans = 2 * i;
         printf(" 2 * %d = %d \n", i, ans);
         i++;
-    }
+    } while (i <= 10);
     return 0;
 }
--- loops.c/tabledw.c
+++ loops.c/tabledw.c
@@ -1,16 +1,17 @@
-// This program will print table of any desired number using  while loop
+// This program will print table of any desired number using do while loop
 #include <stdio.h>
 int main()
 {
-    int i = 1, n;
+    int i = 1, n; // In while and Do while loop initialization is must
     printf("Enter Any Number for Which you want to print Table : ");
     scanf("%d", &n);
-    while (i < 11)
+
+    do
     {
         int ans;
         ans = n * i;
         printf(" %d * %d = %d \n", n, i, ans);
         i++;
-    }
+    } while (i < 11);
     return 0;
 }
