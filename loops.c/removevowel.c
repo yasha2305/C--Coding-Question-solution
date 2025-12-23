b9e091f47e6ae18ca6a04423a8ed7da1a1f09df7
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-23 19:50:43 +0530

    remove vowel from string

---
+++ loops.c/removevowel.c
@@ -0,0 +1,22 @@
+//Remove All Vowels From A String#include <stdio.h>
+#include<stdio.h>
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    scanf(" %[^\n]", str);
+
+    printf("String without vowels: ");
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i]!='a' && str[i]!='e' && str[i]!='i' &&
+            str[i]!='o' && str[i]!='u' &&
+            str[i]!='A' && str[i]!='E' && str[i]!='I' &&
+            str[i]!='O' && str[i]!='U') {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
