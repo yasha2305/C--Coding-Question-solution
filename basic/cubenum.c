23a06e95af996165a0e6fcbc8d455bcdd93cc053
yasha2305 <yasha.sahucse24@ssipmt.com>
2025-12-25 20:12:30 +0530

    Cube of a Number

---
+++ basic/cubenum.c
@@ -0,0 +1,10 @@
+//this code will print a cube of a number
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    printf("Cube = %d", n * n * n);
+    return 0;
+}
